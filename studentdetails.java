6b899f226350f8b29554bdd5e6c1bbd8a4c46b5d
technical2215 <technical2215@gmail.com>
2025-07-25 12:47:44 +0530

    student details using jenkins

---
+++ studentdetails.java
@@ -0,0 +1,10 @@
+public class studentdetails{
+	public static void main(String args[]){
+		String name = "Priyanshu";
+		String rollno="245122749058";
+		String dept="CSE(IOT)";
+		System.out.println("Name of Student:"+name);
+		System.out.println("Roll No.of Student:"+rollno);
+		System.out.println("Department:"+dept);
+	}
+}
