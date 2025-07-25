d20995cfc9a11bfe40c0a92e160562af350f7f70
technical2215 <technical2215@gmail.com>
2025-07-25 12:56:55 +0530

    Update studentdetails.java

--- studentdetails.java
+++ studentdetails.java
@@ -1,10 +1,15 @@
-public class studentdetails{
-	public static void main(String args[]){
-		String name = "Priyanshu";
-		String rollno="245122749058";
-		String dept="CSE(IOT)";
-		System.out.println("Name of Student:"+name);
-		System.out.println("Roll No.of Student:"+rollno);
-		System.out.println("Department:"+dept);
-	}
+public class StudentDetails {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Please provide name, roll number, and branch as arguments.");
+            System.exit(1);
+        }
+        String name = args[0];
+        int rollNo = Integer.parseInt(args[1]);  
+        String branch = args[2];
+        System.out.println("\nStudent Details:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Branch: " + branch);
+    }
 }
